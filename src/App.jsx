6d20d6cebd53da303803adf0d6e7269e273ae7e7
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2024-07-01 12:50:30 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import "./App.css";
 
 import Navigation from "./components/Navigation";
 import HeroSection from "./components/Hero";
+
 const App = () => {
   return (
     <div>
       <Navigation />
+      
       <HeroSection />
     </div>
   );
 };
+
+
 
 export default App;
