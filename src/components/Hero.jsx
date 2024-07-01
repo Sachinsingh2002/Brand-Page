1ef9039735afb3c3e2aeae050c8b54d54139a41f
Sachin Singh <108183252+Sachinsingh2002@users.noreply.github.com>
2024-07-01 12:52:46 +0530

    Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,8 @@ const HeroSection = () => {
           <h1>YOUR FEET DESERVE THE BEST</h1>
           <p>
             YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
-            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
             SHOES.
           </p>
   
@@ -21,7 +22,9 @@ const HeroSection = () => {
               <img src="/images/amazon.png" alt="amazon-logo" />
               <img src="/images/flipkart.png" alt="flipkart-logo" />
             </div>
+          
           </div>
+        
         </div>
         <div className="hero-image">
           <img src="/images/shoe_image.png" alt="hero-image" />
